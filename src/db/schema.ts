a871ea27335dc879b5712cda1d43083cb1324295
vhlongon <vhlongon@gmail.com>
2024-02-11 17:10:24 +0100

    setup app and db

---
+++ src/db/schema.ts
@@ -0,0 +1,22 @@
+import { sql } from 'drizzle-orm';
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+export const events = sqliteTable('events', {
+  id: text('id').notNull().primaryKey(),
+  name: text('name').notNull(),
+  createdAt: text('created_at')
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+});
+
+export const timeSuggestions = sqliteTable('time_suggestions', {
+  id: integer('id').notNull().primaryKey(),
+  eventId: text('event_id')
+    .notNull()
+    .references(() => events.id),
+  time: text('time').notNull(),
+  author: text('author').notNull(),
+  createdAt: text('created_at')
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+});
---
+++ src/env.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    DATABASE_AUTH_TOKEN: string;
+    DATABASE_URL: string;
+  }
+}
