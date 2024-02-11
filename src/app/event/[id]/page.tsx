4ea46e39b5045e058de731cbcc429dae63d82e75
vhlongon <vhlongon@gmail.com>
2024-02-12 00:04:42 +0100

    get data for specific event

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -1,13 +1,73 @@
+import { db } from '@/db/db';
+import { events, timeSuggestions } from '@/db/schema';
+import { eq } from 'drizzle-orm';
+
+type Event = typeof events.$inferSelect;
+type TimeSuggestion = typeof timeSuggestions.$inferSelect;
+type AggrecatedData = Record<
+  string,
+  Event & {
+    timeSuggestions: TimeSuggestion[];
+  }
+>;
+
+const getEventData = async (id: string) => {
+  const rows = await db
+    .select({
+      event: events,
+      timeSuggestion: timeSuggestions,
+    })
+    .from(events)
+    .where(eq(events.id, id))
+    .rightJoin(timeSuggestions, eq(timeSuggestions.eventId, id))
+    .all();
+
+  const result = rows.reduce<AggrecatedData>((acc, row) => {
+    const { event, timeSuggestion } = row;
+
+    if (!event) {
+      return acc;
+    }
+
+    const { id } = event;
+
+    if (!acc[id]) {
+      acc[id] = { ...event, timeSuggestions: [] };
+    }
+    if (timeSuggestion) {
+      acc[id].timeSuggestions.push(timeSuggestion);
+    }
+    return acc;
+  }, {});
+
+  return result[id];
+};
+
 type EventPageProps = {
   params: {
     id: string;
   };
 };
 
-export default function Event({ params }: EventPageProps) {
+export default async function Event({ params }: EventPageProps) {
+  const { name, timeSuggestions } = await getEventData(params.id);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-8">
-      <h1>Event: {params.id}</h1>
+      <div className="card min-w-96 max-w-lg bg-base-100 shadow-xl">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">Event: {name}</h2>
+        </div>
+        <ul className="card-actions justify-center">
+          {timeSuggestions.map(suggestion => (
+            <li key={suggestion.id}>
+              <button className="btn btn-ghost">
+                {new Date(suggestion.time).toLocaleDateString()}
+              </button>
+            </li>
+          ))}
+        </ul>
+        <ul></ul>
+      </div>
     </main>
   );
 }
