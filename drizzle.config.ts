e210b9260ba9bf182fa2feb78225664a6a60adb6
vhlongon <vhlongon@gmail.com>
2024-03-25 18:29:39 +0100

    setup new db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'drizzle-kit';
 
 export default {
   schema: './src/db/schema.ts',
-  out: './migrations',
+  out: './drizzle',
   driver: 'turso',
   dbCredentials: {
     url: process.env.DATABASE_URL,
